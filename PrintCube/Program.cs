8ab1b8d5721c512a0a9305df23e7938bb9a5bbc2
John Atkins <jdatkins@tql.com>
2021-05-19 16:37:01 -0400

    Add project files.

---
+++ PrintCube/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PrintCube
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var i = 1;
+            var cube = 0;
+
+            while (i <= 25)
+            {
+                cube = i * i * i;
+                Console.WriteLine($"The cube of {i} is {cube}.");
+                i += 2;
+            }
+        }
+    }
+}
